a0a12f1d9fa45aefe638967d6890fbbc5a4a1e80
jon-castro <jcastrogarcia@gmail.com>
2025-02-18 04:04:39 -0600

    Work on the game loop

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from constants import *
 def main():
     pygame.init()
     
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+            screen.fill((0,0,0))
+            pygame.display.flip()
+            tick = clock.tick(60)
+            dt = tick/1000
+            print(dt)
     
 if __name__ == "__main__":
     main()
